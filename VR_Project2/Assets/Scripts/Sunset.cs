2adbbb516052d0d51a7674f8c26c0775cbffa40a
Charlie Marlow - 811491261 <cwm36477@uga.edu>
2019-03-29 14:07:14 -0400

    made changes to sunset script

--- VR_Project2/Assets/Scripts/Sunset.cs
+++ VR_Project2/Assets/Scripts/Sunset.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class Sunset : MonoBehaviour
 {
-    public float timeOffset;
-    public float time;
-    public Transform sun;
-    public Light sunLight;
-    public float intensity;
-    public Color fogday = Color.grey;
-    public Color fognight = Color.black;
+    public float timeOffset; // amount of time, in seconds, to offset from sunrise
+    public float time;  
+    public Transform sun;    // directional lights transform
+    public Light sunLight;  // sunlight
+    public float intensity; // how intense the light is
+    public Color fogday = Color.grey;  // color of fog during the day
+    public Color fognight = Color.black;  // color of fog at night
 
-    public int speed;
-    public int days;
-    public float ambientLightMultiplier;
-    public float ambientIntensity;
+    public int speed;   // speed of sunset 
+    public float ambientIntensity;  // intensity of ambient light
     // Start is called before the first frame update
     void Start()
     {
@@ -24,46 +22,50 @@ public class Sunset : MonoBehaviour
 
     IEnumerator sunset()
     {
-        while (true)
+
+        bool isDay = true;
+        while (isDay)
         {
             time += Time.deltaTime * speed;
             float offsetTime = time + timeOffset;
-            Debug.Log("Do something");
-            if(offsetTime >= 54000 && offsetTime <= 58100) {
-                ambientIntensity = (58100f - offsetTime) / 5000f;
+
+            // set up variables for controlling ambient light during sunset
+            float sunsetStart = 54000;
+            float sunsetEnd = 58100;
+            float totalNight = 60000;
+            float scaleDownFactor = 5000;
+
+            if(offsetTime >= sunsetStart && offsetTime <= sunsetEnd) {
+                // scale down ambient lighting during sunset
+                ambientIntensity = (sunsetEnd - offsetTime) / scaleDownFactor;
                 RenderSettings.reflectionIntensity = ambientIntensity;
                 RenderSettings.ambientIntensity = ambientIntensity;
-            }else if(offsetTime >= 58101) {
-
-                Debug.Log("Go black");
+            }else if(offsetTime >= sunsetEnd) {
+                // turn off ambient lighting
                 RenderSettings.reflectionIntensity = 0;
                 RenderSettings.ambientIntensity = 0;
             }
 
+            // set up variables for controlling sun rotation/fog settings
             float secondsInADay = 60 * 60 * 24;
             float halfDay = secondsInADay / 2;
             float quarterDay = halfDay / 2;
-            /*
-            if (time > secondsInADay)
-            {
-                days += 1;
-                time = 0;
-            }*/
 
+            // rotate sun and calculate intensity
             sun.rotation = Quaternion.Euler(new Vector3((offsetTime - quarterDay) / 64000 * 360, 0, 0));
-            /*
-            if (time < halfDay)
-            {
-                intensity = 1 - (halfDay - offsetTime) / halfDay;
-            }
-            else
-            {
-            */
-                intensity = 1 - ((halfDay - offsetTime) / halfDay * -1);
-            //}
+            intensity = 1 - ((halfDay - offsetTime) / halfDay * -1);
 
+            // set fog color
             RenderSettings.fogColor = Color.Lerp(fognight, fogday, intensity * intensity);
             sunLight.intensity = intensity;
+
+            // turn off sun, exit coroutine
+            if(offsetTime >= totalNight)
+            {
+                sunLight.gameObject.SetActive(false);
+                isDay = false;
+            }
+
             yield return null;
         }
         yield return null;
