9c87c00c18497205157370d0f06ac93736be6d36
kaw51647 <46942653+kaw51647@users.noreply.github.com>
2019-03-30 19:52:20 -0400

    Added teleportation

---
+++ VR_Project2/Assets/TeleportStuff/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public VRTeleporter teleporter;
+    public OVRInput.Controller myController;
+
+    void Update()
+    {
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstick, myController)) 
+        {
+            teleporter.ToggleDisplay(true);
+        }
+
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryThumbstick, myController))
+        {
+            teleporter.Teleport();
+            teleporter.ToggleDisplay(false);
+        }
+    }
+}
